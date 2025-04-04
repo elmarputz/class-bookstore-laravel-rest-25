8469a14a1420643b75635f397e9071cbec25c2a6
Elmar Putz <ep@studiomitte.com>
2025-04-04 16:58:08 +0200

    api get methods

--- routes/web.php
+++ routes/web.php
@@ -15,15 +15,6 @@ use App\Models\Book;
 |
 */
 
-Route::get('/books', function () {
-
-    $books = Book::all();
-    return view('books.index', compact('books')
-    );
-});
-
-Route::get('/books/{id}', function ($id) {
-    $book = Book::find($id);
-    // dd($book);
-    return view('books.show', compact('book'));
-});
+Route::get('/', [\App\Http\Controllers\BookController::class, 'index']);
+Route::get('/books', [\App\Http\Controllers\BookController::class, 'index']);
+Route::get('/books/{book}', [\App\Http\Controllers\BookController::class, 'show']);
